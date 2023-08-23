bc0bee09233a6884a664b3320958f3b76c058ee1
Marco Mengoli <marco.mengoli4@gmail.com>
2023-08-23 19:47:44 +0200

    Added first model types

---
+++ instage/instage.api/src/models/Scene.ts
@@ -0,0 +1,10 @@
+import { DeviceScene } from "./DeviceScene";
+
+type Scene = {
+  name: string;
+  devices: { [key: number]: DeviceScene }[];
+};
+
+// TODO: verify that the device addresses are not colliding
+
+export { Scene };
